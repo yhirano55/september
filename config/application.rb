ad60447b01aa72020f41563f9b74775a2b0f8c4c
Yoshiyuki Hirano <yhirano@me.com>
2022-10-22 10:56:32 +0900

    Setup generators

--- config/application.rb
+++ config/application.rb
@@ -20,20 +20,18 @@ Bundler.require(*Rails.groups)
 
 module September
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-
     config.active_job.queue_adapter = :good_job
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.preview true
+      g.system_tests false
+    end
   end
 end
